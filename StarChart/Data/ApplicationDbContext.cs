f5ebadbb09e234060b7156f8ada8813b936cf574
Tore Aurstad <tore.aurstad@hemit.no>
2020-04-25 17:12:50 +0200

    Part 3 of the course.

--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -1,11 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using StarChart.Models;
 
 namespace StarChart.Data
 {
     public class ApplicationDbContext : DbContext
     {
+
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
+            
         }
+
+        public DbSet<CelestialObject> CelestialObjects { get; set; }
+
+
     }
 }
