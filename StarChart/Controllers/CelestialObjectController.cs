5ff4dbd69bc10dda6001208bcff6cb9eaea4c1cc
Tore Aurstad <tore.aurstad@hemit.no>
2020-04-25 17:17:44 +0200

    Adding controller

---
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StarChart.Data;
+
+namespace StarChart.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class CelestialObjectController : ControllerBase
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public CelestialObjectController(DbContextOptions<ApplicationDbContext> options)
+        {
+            _context = new ApplicationDbContext(options);
+        }
+
+    }
+}
